920d61cf5a34a4495587fb85e02d6328aec4930f
Martin Dyring-Andersen <martin@dyring-andersen.dk>
2017-05-10 12:26:03 -0400

    Lower to (and stay at) minimumRate

--- strategies/updateLoan/index.js
+++ strategies/updateLoan/index.js
@@ -13,18 +13,10 @@ module.exports.lowerRateWithTime = function(offer, data, settings, exchangeName,
     // If the loan is older than 'lowerAfterMinutes' old and is still higher than the 'targetRate',
     // make a new loan with the rate lowered by 'lowerByPercent'
     if (age > settings.rateUpdateStrategy.lowerAfterMinutes) {
-	if (offer.rate > data.targetRate) {
-	    offer.rate = data.targetRate;
-	}
-	else {
-	    offer.rate = offer.rate - (offer.rate * settings.rateUpdateStrategy.lowerByPercent / 100);
-	}
-	offer.action = 'update';
-    }
-
-    // cancel if we are now below our minimum rate
-    if (offer.rate < settings.minimumRate) {
-	offer.action = 'cancel';
+        offer.rate = offer.rate - (offer.rate * settings.rateUpdateStrategy.lowerByPercent / 100);
+        if (settings.minimumRate < offer.rate) {
+            offer.action = 'update';
+        }
     }
 
     cb(null, offer);
